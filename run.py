aa4af98ea43f8f352795f70fe68cee3189f41efa
osanam20-commits <osanam2020@gmail.com>
2026-07-10 22:16:41 +0300

    Update run.py
    استبدال ملف

--- run.py
+++ run.py
@@ -1,42 +1,29 @@
 import os
 import telebot
-import requests
-import time
-from flask import Flask
-from threading import Thread
+from flask import Flask, request
 
-# الإعدادات
 TOKEN = "8689943788:AAHOD6jINGiV_g8wHYJ8eZ5SwO6_OLngoYE"
 CHANNEL_ID = "-1004372754611"
-API_KEY = "curl --request GET \
-	--url 'https://football-prediction-api.p.rapidapi.com/api/v2/predictions?market=classic&iso_date=2018-12-01&federation=UEFA' \
-	--header 'Content-Type: application/json' \
-	--header 'x-rapidapi-host: football-prediction-api.p.rapidapi.com' \
-	--header 'x-rapidapi-key: 3ceaa7be00msha38c948056a4052p1fd973jsn92dcc1392590'"
+URL = "https://worldcup-system.onrender.com/" # استبدل اسم-مشروعك برابط موقعك الحقيقي على Render
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
+# --- دالة استقبال الرسائل ---
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+# --- إعداد الويب هوك ---
+bot.remove_webhook()
+bot.set_webhook(url=URL + TOKEN)
+
 @app.route('/')
 def home():
-    return "البوت يعمل!"
-
-# دالة اختبار للرد السريع
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "أنا متصل وأعمل!")
-    print("تم تلقي أمر start والرد عليه")
-
-def check_bot():
-    # هذا السطر يجبر البوت على مسح أي جلسة معلقة في تيليجرام
-    bot.remove_webhook()
-    print("تم حذف الويب هوك القديم")
-    
-    # حلقة التشغيل الأساسية
-    bot.infinity_polling(none_stop=True, skip_pending=True)
+    return "البوت يعمل بنظام Webhook!"
 
 if __name__ == '__main__':
-    # تشغيل البوت في مسار منفصل
-    Thread(target=check_bot).start()
-    # تشغيل خادم Flask
     app.run(host='0.0.0.0', port=10000)
